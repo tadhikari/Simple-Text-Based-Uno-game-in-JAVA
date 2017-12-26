05d397af143eb33eb74e1b6ad9022cf999dc686d
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2017-12-25 23:52:49 -0600

    Update Main.java
    added some comments

--- Uno/src/Main.java
+++ Uno/src/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/*
+		 *Main functions just runs the game by calling the game method. game mthod resides in the Uno class.
+		 */
 	
 		Uno game = new Uno();
 		
